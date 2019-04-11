fe2a0dcad1ce0d2767a10b43f6fcd71c86b137bb
kross <670283005@qq.com>
2019-04-12 01:26:31 +0800

    补充一点注释

--- src/re/RETreeNode.java
+++ src/re/RETreeNode.java
@@ -37,21 +37,37 @@ public abstract class RETreeNode {
         for (int i = 0; i < re.toCharArray().length; i++) {
             char c = re.charAt(i);
             if (c == '(') {
-                // 遇见左括号，要创建一个新的栈出来存放字符
+                // 遇见左括号，创建一个新栈就可以了
                 Stack<RETreeNode> newStack = new Stack<>();
                 stacks.push(newStack);
                 currStack = newStack;
             } else if (c == ')') {
+                // 遇见右括号
+
+                // 先将当前栈的内容全部拼接在一起成一个节点
                 RETreeNode seqNode = popStackToNode(currStack);
+
+                // 调整 当前栈的指针
                 stacks.pop();
                 currStack = stacks.peek();
+
+                // 判断一下当前栈（上个栈）的栈顶元素，是否是一个选择关系节点
                 if (!currStack.isEmpty() && currStack.peek() instanceof RESelectionNode) {
+
+                    // 如果是的话，那么回退下标，因为这次的退栈是为了处理选择节点的右边的部分，而不是与左括号对应的退栈
                     i--;
+
+                    // 将之前拼接在一起的节点存到选择节点的右侧
                     ((RESelectionNode) currStack.peek()).right = seqNode;
                 } else {
+
+                    // 如果是个普通的节点，那么push上去就行了
                     currStack.push(seqNode);
                 }
             } else if (c == '|') {
+
+                // 遇到 选择 关系
+
                 RETreeNode seqNode = popStackToNode(currStack);
                 RESelectionNode selectionNode = new RESelectionNode();
                 selectionNode.left = seqNode;
@@ -78,13 +94,8 @@ public abstract class RETreeNode {
         while (!currStack.isEmpty() || !stacks.isEmpty()) {
             node = popStackToNode(currStack);
 
-            if (!stacks.isEmpty()) {
-                stacks.pop();
-            }
-
-            if (stacks.isEmpty()) {
-                break;
-            }
+            if (!stacks.isEmpty()) stacks.pop();
+            if (stacks.isEmpty()) break;
 
             currStack = stacks.peek();
 
